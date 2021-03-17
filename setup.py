eefe7636c31e13d00a977c7ad10f2c5f09b1b07c
faith <xianzixiang@gmail.com>
2021-03-17 23:28:11 +0800

    add init

--- setup.py
+++ setup.py
@@ -64,7 +64,8 @@ setup(
     version=version,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "telemetry"]),
     zip_safe=False,
-    entry_points={'console_scripts': ['wfb=wfb.base:execute']},
+    entry_points={'console_scripts': ['wfb=wfb.base:execute',
+                                      'wfb-init=wfb_init.init_package:main']},
     package_data={'wfb.conf': ['master.cfg', 'site.cfg']},
     data_files = [('/usr/bin', ['wfb_tx', 'wfb_rx', 'wfb_keygen']),
                   ('/lib/systemd/system', [
---
+++ wfb_init/init_package.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+
+apt_cmd = "apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio"
+
+def main():
+    # implement pip as a subprocess:
+    print('pip install')
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+    'pyroute2', 'twisted'])
+    print('apt install')
+    subprocess.check_output(apt_cmd, shell=True, text=True)
