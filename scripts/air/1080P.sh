8c147e725cb0f7abad5dffc98d545671e7524abe
faith <xianzixiang@gmail.com>
2021-03-18 13:39:38 +0800

    feat: add kill

--- scripts/air/1080P.sh
+++ scripts/air/1080P.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
 
+ps -ef|grep -v grep|grep raspivid|awk '{print $2}'|xargs kill -9
+
+
 raspivid -n  -pf baseline -ex sports -w 1920 -h 1080 -b 1000000 -fps 30 -vf -hf -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=35 ! udpsink sync=false host=127.0.0.1 port=5600"
--- scripts/air/wfb_tx.sh
+++ scripts/air/wfb_tx.sh
@@ -1,1 +1,4 @@
+
+ps -ef|grep -v grep|grep wfb|awk '{print $2}'|xargs kill -9
+
 sudo wfb tx -w wlan1 --nogst
